00601a39dd24717a42496b5a523e5049bc3e65de
freddale4 <sin442@hotmail.com>
2026-01-28 10:26:54 -0300

    Add files via upload

---
+++ map/presence.js
@@ -0,0 +1,45 @@
+/// <reference types="@workadventure/iframe-api-typings" />
+
+/**
+ * github: https://github.com/freddale4/wa
+ * map: https://freddale4.github.io/wa/office.json
+ */
+
+const POWER_AUTOMATE_URL = "https://prod-19.northcentralus.logic.azure.com:443/workflows/fd3d8b7f682e425681c1d5e14b2529fb/triggers/When_an_HTTP_request_is_received/paths/invoke?api-version=2016-10-01&sp=%2Ftriggers%2FWhen_an_HTTP_request_is_received%2Frun&sv=1.0&sig=Xs6Glt1hNgQ8G_6JQLPXO3cHF53Kopp-rgPQqhwcyJ8";
+
+async function sendEvent(eventType, zoneName) {
+    const player = WA.player;
+
+    const payload = {
+        userId: player.id,
+        userName: player.name,
+        eventType: eventType, // ENTER | LEAVE
+        zone: zoneName,
+        eventTime: new Date().toISOString(),
+        source: "WorkAdventure"
+    };
+
+    try {
+        await fetch(POWER_AUTOMATE_URL, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(payload)
+        });
+    } catch (err) {
+        console.error("Erro ao enviar evento:", err);
+    }
+}
+
+WA.onInit().then(() => {
+    const ZONE_NAME = "TRABALHO";
+
+    WA.room.onEnterZone(ZONE_NAME, () => {
+        sendEvent("ENTER", ZONE_NAME);
+    });
+
+    WA.room.onLeaveZone(ZONE_NAME, () => {
+        sendEvent("LEAVE", ZONE_NAME);
+    });
+});
